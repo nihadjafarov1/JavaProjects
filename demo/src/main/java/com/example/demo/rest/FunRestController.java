0ac1e5c57256c4616bcc2c2d8f6c511caa80d1ff
nihadjafarov01 <nihad.ceferov7@gmail.com>
2024-07-17 14:06:18 +0400

    first commit

---
+++ demo/src/main/java/com/example/demo/rest/FunRestController.java
@@ -0,0 +1,16 @@
+package com.example.demo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }
+    @GetMapping("/hy")
+    public String hi() {
+        return "Hello";
+    }
+}
