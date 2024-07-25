058f34e2b5ae7ee41183fe6d9d5b2cdd7e946ce7
nihadjafarov01 <nihad.ceferov7@gmail.com>
2024-07-25 23:54:00 +0400

    Tested Spring Data Rest

--- demorestjpa/src/main/java/com/example/demorestjpa/dao/EmployeeDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.demorestjpa.dao;
-
-import com.example.demorestjpa.entity.Employee;
-
-import java.util.List;
-
-public interface EmployeeDAO {
-    List<Employee> findAll();
-}
--- demorestjpa/src/main/java/com/example/demorestjpa/service/EmployeeService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demorestjpa.service;
-
-import com.example.demorestjpa.entity.Employee;
-
-import java.util.List;
-
-public interface EmployeeService {
-    List<Employee> findAll();
-}
